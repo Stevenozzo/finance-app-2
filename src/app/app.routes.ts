e872180eaf0439739602d59f4f66b9d716083118
Stevenozzo <stevencarlino46@live.com>
2025-02-24 16:39:49 +0100

    bugfixing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserScreenComponent } from './user-screen/user-screen.component';
@@ -9,19 +8,18 @@ import { ProfileComponent } from './profile/profile.component'; // Importa il co
 import { AuthGuard } from './auth.guard'; // Protegge le rotte dopo il login
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  
-  { 
+
+  {
     path: 'user',
     component: UserScreenComponent,
     canActivate: [AuthGuard], // Protegge questa sezione
     children: [
       { path: 'transactions', component: TransactionsHistoryComponent },
       { path: 'add-transaction', component: AddTransactionComponent },
-      { path: 'profile', component: ProfileComponent } // Aggiungi la rotta per il profilo
-    ]
+      { path: 'profile', component: ProfileComponent }, // Aggiungi la rotta per il profilo
+    ],
   },
-  { path: '**', redirectTo: '' } // Rotta di fallback
+  { path: '**', redirectTo: '' }, // Rotta di fallback
 ];
